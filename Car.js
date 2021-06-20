4d836c713aa0885b18d9d055749e25e1d85b3142
yug-dev12 <85817684+yug-dev12@users.noreply.github.com>
2021-06-20 00:27:09 -0700

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,21 @@
+class Car{
+    constructor(speed){
+        this.x = random([100,210,320,440,550,660,770,880,990,1100,1210]);
+        this.y = height-random([150,250,350,950,1050,1150,1750,1850,1950]);
+       
+        this.spt = createSprite(this.x,this.y);
+        
+        this.spt.shapeColor = "red";
+        this.spt.addAnimation("car",carAnimation1);
+        this.spt.scale=0.05;
+
+        if(this.y==height-250 || this.y==height-1050 || this.y==height-1850)
+        {
+            speed=-speed;
+            this.spt.addAnimation("car",carAnimation2);
+        }
+        this.spt.velocityX = speed;
+    }
+
+   
+}
